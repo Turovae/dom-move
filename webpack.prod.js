fee609eb2bde6a58bd1710958c2dedb6e5034291
Turovae <ekaterina.turova@mail.ru>
2023-02-24 23:47:47 +0700

    init project

---
+++ webpack.prod.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { merge } = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({}),
+      new CssMinimizerPlugin({}),
+    ],
+  },
+});
